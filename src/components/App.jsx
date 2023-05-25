00c0439be80e71b6381b2f35a5f7b2e38fbbe3da
Vladyslav Biriukov <vladislavbiriukovsquirrel@gmail.com>
2023-05-25 18:15:03 +0300

    + react-router-dom

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
-export const App = () => {
+
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './Pages/HomePage'
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+    <Routes>
+      <Route path='/' element={<HomePage/>} />
+      
+    </Routes>
+  )
+}
+
+export default App
